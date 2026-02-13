8bb82d16036da91c4876299155ba57a686ef72b4
Prajwal M <prajjumss1234@gmail.com>
2026-02-13 19:49:19 +0530

    First commit

---
+++ calci.py
@@ -0,0 +1,17 @@
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+
+operator=input("Enter operator (+, -, *, /): ")
+
+if operator=='+':
+    res=num1+num2  
+elif operator=='-':
+    res=num1-num2
+    
+elif operator=='*':
+    res=num1*num2
+    
+elif operator=='/':
+     res=num1/num2
+
+print(f"Result of {num1}{operator}{num2} is {res:.2f}")
